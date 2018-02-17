7c5b6e2f48b64bfbb9c1f9c61c9dbf6d461c7aa2
Livia Kobler <livia.kobler@bluewin.ch>
2018-02-17 11:38:57 +0100

    versuch

---
+++ js/events.js
@@ -0,0 +1,11 @@
+let todoList = new TodoList;
+
+let inputEL = document.querySelectorAll('.todo__inputfield');
+let liste = document.getElementById('liste');
+
+inputEL.addEventListener('keypress',function (e) {
+    if (e.value===13 && e.value !== ''){
+        todoList.
+    }
+
+});
--- js/test.js
+++ js/test.js
@@ -1,177 +1,192 @@
-let assert = require('chai').assert;
-let expect = require('chai').expect;
+// Machen wir nur für die node Tests
+if (typeof module !== 'undefined' && module) {
+    assert = require('chai').assert;
+    expect = require('chai').expect;
 
-let Task = require('./todo.js').Task;
-let TodoList = require('./todo.js').TodoList;
+    Task = require('./todo.js').Task;
+    TodoList = require('./todo.js').TodoList;
+}
 
 describe('Todo', function () {
 
-    it('wir sollten ein Task Objekt erzeugen können', function () {
-        let t = new Task;
-        assert.instanceOf(t, Task);
-    });
 
-    it('wir sollten eine Todoliste erzeugen können', function () {
-        let t = new TodoList;
-        assert.instanceOf(t, TodoList);
+    describe('TodoList', function () {
+
+        it('Ein Task sollte nach hinten verschoben werden können', function () {
+            let liste = new TodoList();
+            liste.addTask('Neue Aufgabe');
+            liste.addTask('Neue Aufgabe A');
+            liste.addTask('Neue Aufgabe L');
+            //--- A sollte hier sein
+            liste.addTask('Neue Aufgabe E');
+            liste.addTask('Neue Aufgabe X');
+            liste.addTask('Neue Aufgabe ANDER');
+
+
+            liste.moveBefore(1, 3);
+            // wir schieben von 1 auf bevor 3 (=>2)
+            expect(liste.tasks[2].text).equal('Neue Aufgabe A');
+            expect(liste.tasks[2].position).equal(3.5);
+
+            liste.moveBefore(0, 3);
+            expect(liste.tasks[2].text).equal('Neue Aufgabe');
+            expect(liste.tasks[2].position).equal(3.75);
+
+            console.log(liste.tasks)
+        });
+
+
+        it('Ein Task sollte nach vorne verschoben werden können', function () {
+            let liste = new TodoList();
+            liste.addTask('Neue Aufgabe');
+            liste.addTask('Neue Aufgabe A');
+            liste.addTask('Neue Aufgabe L');
+            //--- A sollte hier sein
+            liste.addTask('Neue Aufgabe E');
+            liste.addTask('Neue Aufgabe X');
+            liste.addTask('Neue Aufgabe ANDER');
+            console.log(liste.tasks);
+
+
+            liste.moveBefore(5, 0);
+            // wir schieben von 1 auf bevor 3 (=>2)
+            expect(liste.tasks[0].text).equal('Neue Aufgabe ANDER');
+            expect(liste.tasks[0].position).equal(0.5);
+
+        });
+
+        it('Der Text eines Tasks in der Liste sollte aktualisert werden können', function () {
+            let liste = new TodoList();
+            liste.addTask('Neue Aufgabe');
+            liste.addTask('Neue Aufgabe B');
+            let task = liste.addTask('Neue Aufgabe C');
+
+            liste.updateTaskText(1, "neuer Text");
+            expect(liste.tasks[1].text).equal("neuer Text");
+            liste.tasks[1].text = "Hi";
+            expect(liste.tasks[1].text).equal("Hi");
+
+            task.text = "3";
+            expect(liste.tasks[2].text).equal("3");
+        });
+
+
+        it('Status eines Tasks aus Liste auf erledigt und unerledigt setzen können', function () {
+            let liste = new TodoList();
+            liste.addTask('Neue Aufgabe');
+            liste.addTask('Neue Aufgabe B');
+            let task = liste.addTask('Neue Aufgabe C');
+
+            liste.checkTask(1);
+            expect(liste.tasks[1].erledigt).equal(true);
+
+            task.check();
+            expect(liste.tasks[2].erledigt).equal(true);
+
+            liste.uncheckTask(1);
+            expect(liste.tasks[1].erledigt).equal(false);
+
+        });
+
+
+        it('Wir sollten einen Task der Liste hinzufügen können', function () {
+            let liste = new TodoList();
+            liste.addTask('Neue Aufgabe');
+            expect(liste.tasks.length).equals(1);
+            liste.addTask('Neue Aufgabe B');
+            expect(liste.tasks.length).equals(2);
+
+            expect(liste.tasks[0].text).equal("Neue Aufgabe");
+            expect(liste.tasks[1].text).equal("Neue Aufgabe B");
+
+        });
+
+        it('Neue Tasks sollten die Position des letzten Tasks +1 haben', function () {
+            let liste = new TodoList();
+
+            liste.addTask('Neue Aufgabe');
+            expect(liste.tasks[0].position).equal(1);
+
+            liste.addTask('Neue Aufgabe B');
+            expect(liste.tasks[1].position).equal(2);
+            liste.tasks[1].position = 202;
+            expect(liste.tasks[1].position).equal(202);
+
+            liste.addTask('Neue Aufgabe C');
+            expect(liste.tasks[2].position).equal(203);
+        });
+
+        it('Ein Task soll entfernt werden können', function () {
+            let liste = new TodoList();
+            liste.addTask('Neue Aufgabe A');
+            liste.addTask('Neue Aufgabe B');
+            liste.addTask('Neue Aufgabe C');
+
+            liste.removeTask(1);
+            expect(liste.tasks.length).equal(2);
+            expect(liste.tasks[1].text).equal('Neue Aufgabe C');
+        });
+
     });
 
     describe('Task', function () {
-        it('Wir sollten eine ID, einen Text, eine Position und erledigt setzen können', function () {
-            let task = new Task;
-            expect(task).to.have.property('_text');
-            expect(task).to.have.property('id');
-            expect(task).to.have.property('erledigt');
-            expect(task).to.have.property('_position');
+
+        it('wir sollten ein Task Objekt erzeugen können', function () {
+            let t = new Task;
+            assert.instanceOf(t, Task);
         });
 
-        it('Text sollte ein String sein', function () {
-            let task = new Task;
-            task.text = 'Beispieltext';
-            expect(task.text).to.be.a('string');
-            console.log(task.text + '--> typeof:' + typeof task.text);
-
-            task.text = 33;
-            expect(task.text).to.be.a('string');
-            console.log(task.text + '--> typeof:' + typeof task.text);
-
+        it('wir sollten ein TodoList Objekt erzeugen können', function () {
+            let t = new TodoList;
+            assert.instanceOf(t, TodoList);
         });
 
-        it('Position sollte eine Nummer sein ', function () {
-            let task = new Task;
-            task.position = 2;
-            expect(task.position).to.be.a('number');
-            console.log(task.position + '--> typeof:' + typeof task.position);
-
-            task.position = 'Beispieltext';
-            expect(task.position).to.be.a('number');
-            console.log(task.position + '--> typeof:' + typeof task.position);
+        it('Todolist sollte ein Attribut tasks (array) mit Tasks haben', function () {
+            let tl = new TodoList;
+            assert.typeOf(tl.tasks, 'array');
         });
 
-        it('ein Task sollte auf erledigt gesetzt werden können', function () {
-            let task = new Task;
-            task.checkTask();
-            task.uncheckTask();
-            task.checkTask();
-            expect(task.erledigt).to.equal(true);
-
-
-        });
-        it('ein Task sollte auf NICHT erledigt gesetzt werden können', function () {
-            let task = new Task;
-            task.uncheckTask();
-            task.checkTask();
-            task.uncheckTask();
-            expect(task.erledigt).to.equal(false);
+        it('Tasks sollten id,text,erledigt,position haben', function () {
+            let t = new Task;
+            expect(t.id).to.not.equal(undefined);
+            expect(t.text).to.not.equal(undefined);
+            expect(t.erledigt).to.not.equal(undefined);
+            expect(t.position).to.not.equal(undefined);
         });
 
-    });
-
-    describe('TodoList', function () {
-        it('es sollten tasks enthalten können', function () {
-            let liste = new TodoList;
-            expect(liste.tasks).to.be.an('array');
+        it('Sollte einen Task direkt mit Text erzeugen können', function () {
+            let t = new Task("Textbeispiel");
+            expect(t.text).equal("Textbeispiel");
         });
 
-        it('Wir sollten einen neuen Task hinzufügen können', function () {
-            let liste = new TodoList;
-            liste.addTask('neue Aufgabe 1');
-            expect(liste.tasks.length).to.equal(1);
-            liste.addTask('neue Aufgabe 2');
-            expect(liste.tasks.length).to.equal(2);
-
-            expect(liste.tasks[0].text).to.equal('neue Aufgabe 1');
-            expect(liste.tasks[1].text).to.equal('neue Aufgabe 2');
+        it('Sollte einen Task als erledigt markieren können', function () {
+            let t = new Task("Textbeispiel");
+            t.check();
+            expect(t.erledigt).equal(true);
         });
 
-        it('ein neuer Task sollte auf die letzte Position gesetzt werden', function () {
-            let liste = new TodoList;
-            liste.addTask('neue Aufgabe 1');
-            expect(liste.tasks[0].position).to.equal(1);
-
-            liste.addTask('neue Aufgabe 2');
-            expect(liste.tasks[1].position).to.equal(2);
-
-            liste.addTask('neue Aufgabe 3');
-            liste.tasks[2].position =202;
-            expect(liste.tasks[2].position).to.equal(202);
-
-            liste.addTask('neue Aufgabe 4');
-            expect(liste.tasks[3].position).to.equal(203);
+        it('Sollte einen Task als NICHT erledigt markieren können', function () {
+            let t = new Task("Textbeispiel");
+            t.uncheck();
+            expect(t.erledigt).equal(false);
+            t.check();
+            t.uncheck();
+            expect(t.erledigt).equal(false);
         });
 
-        it('ein Task in der Liste sollte auf "erledigt", "NICHT erledigt" gesetzt werden können', function () {
-            let liste = new TodoList;
-            liste.addTask('neue Aufgabe 1');
-            liste.addTask('neue Aufgabe 2');
-            liste.addTask('neue Aufgabe 3');
-            liste.addTask('neue Aufgabe 4');
-            liste.addTask('neue Aufgabe 5');
-            liste.addTask('neue Aufgabe 6');
-
-            liste.solveTask(5);
-            expect(liste.tasks[5].erledigt).to.equal(true);
-
-            liste.unsolveTask(5);
-            expect(liste.tasks[5].erledigt).to.equal(false);
+        it('sollte den Text aktualisieren können', function () {
+            let t = new Task("Textbeispiel");
+            t.text = "**";
+            expect(t.text).equal("**")
         });
 
-        it('Wir sollten einen Task löschen können', function () {
-            let liste = new TodoList;
-            liste.addTask('neue Aufgabe 1');
-            liste.addTask('neue Aufgabe 2');
-            liste.addTask('neue Aufgabe 3');
-            liste.addTask('neue Aufgabe 4');
-            liste.addTask('neue Aufgabe 5');
-            liste.addTask('neue Aufgabe 6');
-
-            liste.removeTask(4);
-            expect(liste.tasks.length).to.equal(5);
-            expect(liste.tasks[4].text).to.equal('neue Aufgabe 6');
-        });
-
-        it('wir sollten einen Task ändern können', function () {
-            let liste = new TodoList;
-            liste.addTask('neue Aufgabe 1');
-            liste.addTask('neue Aufgabe 2');
-            liste.addTask('neue Aufgabe 3');
-            liste.addTask('neue Aufgabe 4');
-            liste.addTask('neue Aufgabe 5');
-            liste.addTask('neue Aufgabe 6');
-
-            liste.updateTask(4,'neuer Text');
-            expect(liste.tasks[4].text).to.equal('neuer Text');
-        });
-
-        it('Wir sollten einen Task nach vorne vor einen anderen schieben können', function () {
-            let liste = new TodoList;
-            liste.addTask('neue Aufgabe 1');
-            liste.addTask('neue Aufgabe 2');
-            liste.addTask('neue Aufgabe 3');
-            liste.addTask('neue Aufgabe 4');
-            liste.addTask('neue Aufgabe 5');
-            liste.addTask('neue Aufgabe 6');
-
-            liste.moveBefore(3,2);
-            expect(liste.tasks[1].text).to.equal('neue Aufgabe 4');
-            console.log(liste.tasks);
-        });
-
-        it('Wir sollten einen Task nach hinten vor einen anderen schieben können', function () {
-            let liste = new TodoList;
-            liste.addTask('neue Aufgabe 1');
-            liste.addTask('neue Aufgabe 2');
-            liste.addTask('neue Aufgabe 3');
-            liste.addTask('neue Aufgabe 4');
-            liste.addTask('neue Aufgabe 5');
-            liste.addTask('neue Aufgabe 6');
-
-            liste.moveBefore(1,5);
-            console.log(liste.tasks);
-            expect(liste.tasks[5].text).to.equal('neue Aufgabe 1');
-
+        it('Es sollte eine neue Positionsnummer zugewiesen werden können', function () {
+            let t = new Task;
+            t.position = 7.5;
+            expect(t.position).equal(7.5);
+            t.position = "Banane";
+            expect(t.position).equal(7.5)
         });
 
     });
 });
-
