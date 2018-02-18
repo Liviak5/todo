e54586fe499b674a12458b9e721e76303ebdbe65
Livia Kobler <livia.kobler@bluewin.ch>
2018-02-18 04:38:28 +0100

    aufgeräumt

--- js/todo.js
+++ js/todo.js
@@ -4,18 +4,24 @@ let todoList = new TodoList();
 
 ready(() => {
 
-  let taskInputFields = document.querySelectorAll('.todo__inputfield');
-  let liste = document.getElementById('liste');
 
-  taskInputFields.forEach((inputEl) => {
-    // Auf Enter Taste hören
-    inputEl.addEventListener('keypress', e => {
-      if (e.keyCode === 13 && inputEl.value !== '') {
-        // task der Liste hinzufügen
-        todoList.addTask(inputEl.value);
-        // input wieder leeren
-        inputEl.value = '';
-      }
+    /**
+     * Wenn Enter gedrückt wird und der Inhalt nicht leer ist, wird der Todoliste ein neuer Task hinzugefügt
+     * @type {NodeListOf<Element>}
+     */
+    let taskInputFields = document.querySelectorAll('.todo__inputfield');
+    let liste = document.getElementById('liste');
+
+    taskInputFields.forEach((inputEL) => {
+        inputEL.addEventListener('keypress', e => {
+            if (e.keyCode === 13 && inputEL.value !== '') {
+                todoList.addTask(inputEL.value);
+                inputEL.value = '';
+            }
+        });
     });
-  });
+
+
+
+
 });
